8bae8e51dd6200d585dbd3056ca1c68388632c14
Travis Holland <travisjholland@gmail.com>
2022-01-04 21:51:31 -0800

    hardhat.config update

--- my-wave-portal/hardhat.config.js
+++ my-wave-portal/hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/DMYChcCLAngLS2xl1CZovsF-04OJh4m9",
+      accounts: ["489ec64e85ba0527bb202eb6cc8d85daec096dc7c37a450cf6482d306356cd77"],
+    },
+  },
 };
